09811db0426798177fd367bbe8a983365a979477
Carly L <carlylynnla@gmail.com>
2020-05-24 14:57:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   :home 
     {:team_name => "Brooklyn Nets"}
     {colors => ["Black", "White"]}
-    {:players learn [
+    {:players [
       {:player_name => "Alan Anderson",
       :number => "0",
       :shoe => "16",
